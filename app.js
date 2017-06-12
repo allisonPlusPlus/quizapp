d418a71f7c55a4be6a65ca47631f172a0ac470a2
theallisonbrett <theallisonbrett@gmail.com>
2017-06-12 11:39:01 -0700

    Add question 4

--- app.js
+++ app.js
@@ -1,22 +1,27 @@
-var questions =
-{
-  Q1 : {
-    question: "Which woman was the lead software engineer on the Apollo?",
-    answer: "Margaret Hamilton",
-    choices: ["Margaret Hamilton", "Grace Hopper", "Annie Easley", "Mary Jackson"]
-  },
+var questions = {
+    Q1: {
+        question: "Which woman was the lead software engineer on the Apollo?",
+        answer: "Margaret Hamilton",
+        choices: ["Margaret Hamilton", "Grace Hopper", "Annie Easley", "Mary Jackson"]
+    },
 
-  Q2 : {
-    question: "Which woman is credited for coining the term 'computer bug' ?",
-    answer: "Grace Hopper",
-    choices: ["Ada Lovelace", "Grace Hopper", "Anita Borg", "Lynn Conway"]
-  },
+    Q2: {
+        question: "Which woman is credited for coining the term 'computer bug' ?",
+        answer: "Grace Hopper",
+        choices: ["Ada Lovelace", "Grace Hopper", "Anita Borg", "Lynn Conway"]
+    },
 
-  Q3: {
-    question: "Which woman is said to be the world's first computer programmer?",
-    answer: "Ada Lovelace",
-    choices: ["Grace Hopper", "Radia Perlman", "Ada Lovelace", "Anita Borg"]
-  },
+    Q3: {
+        question: "Which woman is said to be the world's first computer programmer?",
+        answer: "Ada Lovelace",
+        choices: ["Grace Hopper", "Radia Perlman", "Ada Lovelace", "Anita Borg"]
+    },
+
+    Q4: {
+      question: "Which mathematician was not portrayed in the movie Hidden Figures?",
+      answer: "Grace Hopper",
+      choices: ["Dorothy Vaughan", "Grace Hopper",  "Mary Jackson",  "Katherine Johnson"]
+    },
 
 
 };
