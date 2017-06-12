df49a3185f614d4b1c46a4bd32b39e339889204c
theallisonbrett <theallisonbrett@gmail.com>
2017-06-12 11:31:45 -0700

    Add question 2

--- app.js
+++ app.js
@@ -4,6 +4,13 @@ var questions =
     question: "Which woman was the lead software engineer on the Apollo?",
     answer: "Margaret Hamilton",
     choices: [ "Margaret Hamilton", "Grace Hopper", "Annie Easley", "Mary Jackson"]
-  }
+  },
+
+  Q2 : {
+    question: "Which woman is credited for coining the term 'computer bug' ?",
+    answer: "Grace Hopper",
+    choices: ["Ada Lovelace", "Grace Hopper", "Anita Borg", "Lynn Conway"]
+  },
+
 
 };
