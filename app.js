8fdc3d1c6f9bfbccb639c1f8facfe6d4ae18ecda
theallisonbrett <theallisonbrett@gmail.com>
2017-06-12 11:26:05 -0700

    Add questions object and q1

---
+++ app.js
@@ -0,0 +1,9 @@
+var questions =
+{
+  Q1 : {
+    question: "Which woman was the lead software engineer on the Apollo?",
+    answer: "Margaret Hamilton",
+    choices: [ "Margaret Hamilton", "Grace Hopper", "Annie Easley", "Mary Jackson"]
+  }
+
+};
